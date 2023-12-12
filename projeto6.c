0dd041af0f84400bca555718b83188762860dff3
MonicaPeixotoAraujo <153565709+MonicaPeixotoAraujo@users.noreply.github.com>
2023-12-11 21:33:58 -0300

    Opção de saída
    Incluso no sistema a opção de saída para o usuário.

--- projeto6.c
+++ projeto6.c
@@ -130,6 +130,7 @@ int main()
 	   printf ("\t1 - Registrar nomes\n");
 	   printf ("\t2 - Consultar nomes\n");
 	   printf ("\t3 - Deletar nomes\n\n");
+	   printf ("\t4 - Sair do sistema\n\n");
 	   printf ("opcao:"); //Fim menu
 	
 	   scanf("%d",&opcao); //armazenamento a escolha do usuário
@@ -149,6 +150,11 @@ int main()
 		case 3:
 		deletar(); //chamada de funções
 		break; 
+		
+		case 4:
+		printf ("Obrigado por utilizar o sistema!\n");
+		return 0;
+		break;
 		
 		default:
 		printf ("Essa opção não está disponível.\n");
